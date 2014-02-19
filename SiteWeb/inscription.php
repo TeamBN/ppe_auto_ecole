1db5603498592b2bdaddbf70e08bfe61141f2acd
chocoow <darid.jeremy@gmail.com>
2014-02-19 22:27:42 +0100

    Site Web Premier jet Mise en Partage sur Github

---
+++ SiteWeb/inscription.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html style="height:100%">
+
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=windows-1252">
+	<link rel="stylesheet" href="style.css" />
+	<title>CastellaneAuto - Permis de conduire - Code de la route</title>
+</head>
+
+<body>
+	<header>
+		<div id="bloc_header">
+			<a href="index.htm">
+				<img src="images/Logo.png" alt="Logo CastellaneAuto" id="logo" border="0"/>
+			</a>
+		</div>
+		<!--------------------------MON COMPTE----------------------------------------------------------------------------
+		<div id="mon_compte">
+		</div>------------------------------------------------------------------------------------------------------------->
+	</header>
+	<body>
+		<div id="contenu_body">
+			<section class="fond_bloc">
+				<div id="formulaire">
+                    <form method=post action=valid.php>
+                        <label>Login : <input type="login" name="login"/> *</label><br/> 
+                        <label>Mot de passe : <input type="password" name="pass"/> *</label><br/> 
+                        <label>Confirmation du mot de passe : <input type="password" name="pass2"/> *</label><br/> 
+                        <label>Nom : <input type="text" name="nom"/> *</label><br/> 
+                        <label>Prénom : <input type="text" name="prenom"/> *</label><br/> 
+                        <label>Adresse : <input type="text" name="adresse"/> *</label> <br/> 
+                        <label>Code Postal : <input type="text" name="cp"/> *</label> <br/> 
+                        <label>Ville : <input type="text" name="ville"/> *</label> <br/> 
+                        <label>Mail : <input type="text" name="mail"/> *</label><br> <br/> 
+                        <input type="submit" value="S'inscrire"/>
+                        <br><label style="font-style:italic; font-size: 10px;">* Ces champs sont obligatoires</label>
+                </div> 
+			</section>
+		</div>
+	</body>
+	<div id="footer">
+		<div id="bloc_footer">
+			<table width="100%" height="100%" border="0" cellpadding="0" cellspacing="0">
+				<tr align="center">
+					<td valign="top"><a href="#"><h3>Mention légale</h3></a></td>
+					<td width="1">
+						<div id="sep_footer">
+						</div>
+					</td>
+					<td valign="top"><a href="#"><h3>Réseaux sociaux</h3></a></td>
+					<td width="1">
+						<div id="sep_footer">
+						</div>
+					</td>
+					<td valign="top"><a href="#"><h3>Contact</h3></a></td>
+				</tr>
+			</table>			
+		</div>
+	</div>
+</body>
+</html>
