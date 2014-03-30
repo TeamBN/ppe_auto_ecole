54e937dca7bf157644acef4c6278df96596a1b6d
mabree <ddlfoster123@gmail.com>
2014-03-30 18:42:56 +0200

    Rajout de "cascade sur etudiant et salarie pour l'inverse de l'héritage

--- Base de donnee/bdd.sql
+++ Base de donnee/bdd.sql
@@ -4,15 +4,15 @@ CREATE DATABASE autoecole CHARACTER SET 'utf8';
 use autoecole
 
 CREATE TABLE Client (
-IdC int NOT NULL AUTO_INCREMENT Primary key
+IdC int(3) NOT NULL AUTO_INCREMENT Primary key
 )
 ENGINE = InnoDB;
 
 CREATE TABLE ExamenP (
 IdExaP int NOT NULL AUTO_INCREMENT Primary key,
 Date_Inscri_Permis DATE NOT NULL,
-Nb_Passage_Permis int(1) NOT NULL DEFAULT 0,
-Resultat ENUM ('oui', 'non'),
+Nb_Passage_Permis int(1) DEFAULT 0,
+ResultatP ENUM ('oui', 'non'),
 IdC int NOT NULL, FOREIGN KEY (IdC) REFERENCES Client (IdC)
 )
 ENGINE = InnoDB;
@@ -22,13 +22,13 @@ CREATE TABLE ExamenC (
 IdExaC int not null AUTO_INCREMENT Primary key,
 Date_Inscri_Code DATE NOT NULL,
 Nb_Passage_Code int DEFAULT 0,
-Resultat ENUM ('oui', 'non'),
+ResultatC ENUM ('oui', 'non'),
 IdC int not null, FOREIGN KEY (IdC) REFERENCES Client (IdC)
 )
 ENGINE = InnoDB;
 
 CREATE TABLE Moniteur (
-NumM int(2) NOT NULL Auto_Increment Primary key,
+IdM int(2) NOT NULL Auto_Increment Primary key,
 NomM varchar(25) not null,
 PrenomM varchar(25) not null
 )
@@ -48,7 +48,7 @@ CREATE TABLE Lecon (
 IdNumLE int(3) not null AUTO_INCREMENT Primary key,
 NomLE varchar(20),
 TypeLE int,
-NumM int(11) not null, Foreign Key (NumM) References Moniteur (NumM),
+IdM int(11) not null, Foreign Key (IdM) References Moniteur (IdM),
 IdC int not null, Foreign Key (IdC) References Client (IdC),
 IdV int(3) not null, FOREIGN KEY (IdV) REFERENCES Voiture (IdV)
 )
@@ -56,7 +56,8 @@ ENGINE = InnoDB;
 
 CREATE TABLE Planning (
 IdNumLE int(3), Foreign Key (IdNumLE) References Lecon (IdNumLE),
-NumM int(2), Foreign Key (NumM) References Moniteur (NumM),
+IdM int(2), Foreign Key (IdM) References Moniteur (IdM),
+IdC int(3), Foreign Key (IdC) References Client (IdC),
 D_Debut DATETIME not null,
 D_Fin DATETIME not null
 )
@@ -69,39 +70,35 @@ Primary Key (IdV, Mois), FOREIGN KEY (Idv) REFERENCES voiture (IdV)
 )
 ENGINE = InnoDB;
 
-CREATE TABLE Etablissement (
-IdEta int(3) not null Auto_Increment Primary key,
-NomEta varchar(30),
-AdresseEta varchar(128),
-CpET char(5) NOT NULL,
-VilleET varchar (20) NOT NULL
-)
-Engine = InnoDB;
 
 CREATE TABLE Etudiant (
-IdC int not null auto_increment PRIMARY KEY,
+IdC int(3) not null Auto_INCREMENT Primary Key,
 NomE varchar(25) NOT NULL,
 PrenomE varchar (25) NOT NULL,
 AdresseE varchar(128) NOT NULL,
 CpE char(5) NOT NULL,
 VilleE varchar (20) NOT NULL,
-DateNaiss DATE,
+DateNaiss DATE NOT NULL,
 Telephone char(10) NOT NULL,
 nom_formation varchar (30),
 taux_reduction int(3) DEFAULT 15,
-IdEta int(3) not null, FOREIGN KEY (IdEta) REFERENCES Etablissement ( IdETA)
+NomEta varchar (30) not null,
+FOREIGN KEY (IdC) REFERENCES Client(IdC)
+ON DELETE CASCADE
 )
 Engine = InnoDB;
 
 CREATE TABLE Salarie (
-IdC int not null auto_increment PRIMARY KEY,
+IdC int(3) not null AUTO_INCREMENT Primary Key,
 NomS varchar(25) NOT NULL,
 PrenomS varchar (25) NOT NULL,
 AdresseS varchar(128) NOT NULL,
 CpS char(5) NOT NULL,
 VilleS varchar (20) NOT NULL,
-DateNaiss DATE,
+DateNaiss DATE NOT NULL,
 Telephone char(10) NOT NULL,
-nom_entreprise varchar(30) default 'SansEmploi'
+nom_entreprise varchar(30) default 'SansEmploi',
+FOREIGN KEY (IdC) REFERENCES Client(IdC)
+ON DELETE CASCADE
 )
 Engine = InnoDB;
