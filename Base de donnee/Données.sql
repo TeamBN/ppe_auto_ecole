b69dbcef473648ff5286d9c27398214aadb71441
mabree <ddlfoster123@gmail.com>
2014-03-30 18:44:39 +0200

    Insert sur etudiant + salarie

--- Base de donnee/Données.sql
+++ Base de donnee/Données.sql
@@ -1,37 +1,37 @@
 INSERT INTO Etudiant VALUES
-(default, 'Daryd', 'Jeremy', '12 rue Gambetta', '75013', 'Paris', '1993-01-20', '0643275648', 'BTS', 00, 'IRIS'),
-(default, 'Enaus', 'Clara', '15 rue du Wank', '75012', 'Paris', '1991-11-05', '0646521895', 'BTS', 00, 'IRIS'),
-(default, 'Mateesson', 'Amaury', '37 rue Perdue', '75715', 'Paris', '1992-09-14', '0658931546', 'BTS', 00, 'IRIS'),
-(default, 'Dupont', 'Pierre', '113 avenue de France', '75009', 'Paris', '1994-02-17', '0652648519', 'Licence', 00, 'UPMC'),
-(default, 'Chaulti', 'Melody', '92 rue Husson', '75019', 'Paris', '1996-08-11', '0646821594', 'BACCALAUREAT', 00, 'Albert_Daumun'),
-(default, 'Clerec', 'Jean', '31 avenue Four', '75018', 'Paris', '1988-06-11', '0620375621', 'Master', 00, 'UPEC'),
-(default, 'Fourier', 'David', '87 boulevard des mares', '75014', 'Paris', '1982-05-01', '0668914537', 'Master', 00, 'Paris_Descartes'),
-(default, 'Trong', 'Camille', '64 rue Mirage', '75004', 'Paris', '1994-03-21', '0642138437', 'Licence',00, 'Sorbonne-Nouvelle'),
-(default, 'Fati Abd Al Mulah', 'Muhammed', '48 rue diderot', '75007', 'Paris', '1996-01-13', '0618652479', 'BACCALAUREAT', 00, 'Charlemagne'),
-(default, 'Rousseau', 'Xavier', '218 rue des Charpentiers', '75012', 'Paris', '1990-08-20', '0649851627', 'Licence', 00, 'Paris_Descartes'),
-(default, 'Lipouma', 'Richard', '152 rue des peupliers', '75013', 'Paris', '1990-10-11', '0628451795', 'Licence', 00, 'UPMC'),
-(default, 'DaSilva', 'Melanie', '68 boulevard des honores', '75006', 'Paris', '1991-04-27', '0628495247', 'Master', 00, 'UPMC'),
-(default, 'Antoluni', 'Nicolas', '02 rue du marechal leclerc', '75005', 'Paris', '1992-01-09', '0650221857', 'Licence', 00, 'EGPI'),
-(default, 'Stefany', 'Mathieu', '01 avenue de gaulle', '75003', 'Paris', '1989-02-15', '0624356481', 'Master', 00, 'ENSAPLV'),
-(default, 'Fauster', 'Pavel', '25 rue de l''Union', '75002', 'Paris', '1992-07-19', '0619956418', 'BTS', 00, 'IRIS'),
-(default, 'Lorgne', 'Lydia', '41 rue doobi', '75020', 'Paris', '1989-08-11', '0614253485', 'BTS', 00, 'EFDF');
+(' ', 'Daryd', 'Jeremy', '12 rue Gambetta', '75013', 'Paris', '1993-01-20', '0643275648', 'BTS', 00, 'IRIS'),
+(' ', 'Enaus', 'Clara', '15 rue du Wank', '75012', 'Paris', '1991-11-05', '0646521895', 'BTS', 00, 'IRIS'),
+(' ', 'Mateesson', 'Amaury', '37 rue Perdue', '75715', 'Paris', '1992-09-14', '0658931546', 'BTS', 00, 'IRIS'),
+(' ', 'Dupont', 'Pierre', '113 avenue de France', '75009', 'Paris', '1994-02-17', '0652648519', 'Licence', 00, 'UPMC'),
+(' ', 'Chaulti', 'Melody', '92 rue Husson', '75019', 'Paris', '1996-08-11', '0646821594', 'BACCALAUREAT', 00, 'Albert_Daumun'),
+(' ', 'Clerec', 'Jean', '31 avenue Four', '75018', 'Paris', '1988-06-11', '0620375621', 'Master', 00, 'UPEC'),
+(' ', 'Fourier', 'David', '87 boulevard des mares', '75014', 'Paris', '1982-05-01', '0668914537', 'Master', 00, 'Paris_Descartes'),
+(' ', 'Trong', 'Camille', '64 rue Mirage', '75004', 'Paris', '1994-03-21', '0642138437', 'Licence',00, 'Sorbonne-Nouvelle'),
+(' ', 'Abd Al Mulah', 'Muhammed', '48 rue diderot', '75007', 'Paris', '1996-01-13', '0618652479', 'BACCALAUREAT', 00, 'Charlemagne'),
+(' ', 'Rousseau', 'Xavier', '218 rue des Charpentiers', '75012', 'Paris', '1990-08-20', '0649851627', 'Licence', 00, 'Paris_Descartes'),
+(' ', 'Lipouma', 'Richard', '152 rue des peupliers', '75013', 'Paris', '1990-10-11', '0628451795', 'Licence', 00, 'UPMC'),
+(' ', 'DaSilva', 'Melanie', '68 boulevard des honores', '75006', 'Paris', '1991-04-27', '0628495247', 'Master', 00, 'UPMC'),
+(' ', 'Antoluni', 'Nicolas', '02 rue du marechal leclerc', '75005', 'Paris', '1992-01-09', '0650221857', 'Licence', 00, 'EGPI'),
+(' ', 'Stefany', 'Mathieu', '01 avenue de gaulle', '75003', 'Paris', '1989-02-15', '0624356481', 'Master', 00, 'ENSAPLV'),
+(' ', 'Fauster', 'Pavel', '25 rue de l''Union', '75002', 'Paris', '1992-07-19', '0619956418', 'BTS', 00, 'IRIS'),
+(' ', 'Lorgne', 'Lydia', '41 rue doobi', '75020', 'Paris', '1989-08-11', '0614253485', 'BTS', 00, 'EFDF');
 
 
 
 
-
-INSERT INTO Etudiant(IdC, NomE, PrenomE, AdresseE, CpE, VilleE, DateNaiss, Telephone, nom_formation, NomEta) VALUES
-(default, 'Daryd', 'Jeremy', '12 rue Gambetta', '75013', 'Paris', '1993-01-20', '0643275648', 'BTS', 'IRIS');
-
-
-
-
-
-
-
-
-
-
-
-INSERT INTO Etudiant VALUES
-(default, 'Enaus', 'Clara', '15 rue du Wank', '75012', 'Paris', '1991-11-05', '0646521895', 'BTS', 00, 'IRIS');
+INSERT INTO Salarie VALUES
+(' ', 'Courtot', 'Bertrand', '74 avenue Legrand', '75002', 'Paris', '1975-05-29', '0145758124', 'GFIs Info'),
+(' ', 'Delpra', 'Jeannine', '12 rue du monde', '75004', 'Paris', '1990-08-16', '0158123482', 'ENERP'),
+(' ', 'Courtot', 'Adele', '74 avenue Legrand', '75002', 'Paris', '1978-07-02', '0145758124', 'Sanofri'),
+(' ', 'Weinstein', 'Chloe', '61 rue Raymond du Temple', '75018', 'Paris', '1986-05-17', '0178146529', 'Cramson'),
+(' ', 'Subatic', 'Helena', '235 avenue Forbin', '75001', 'Paris', '1962-03-12', '0158791234', 'Dorcel Industries'),
+(' ', 'Quormat', 'Gilles', '168 rue Fibone', '75010', 'Paris', '1980-01-31', '0143285682', 'Zinko'),
+(' ', 'Ymbra', 'Xavier', '87 rue du Moule', '75014', 'Paris', '1961-05-11', '0132816475', ''),
+(' ', 'Sorni', 'Edgard', '157 rue Marbou', '75012', 'Paris', '1963-04-16', '0152348561', 'Carmeli'),
+(' ', 'Efrou', 'Christine', '50 boulevard Terru', '75003', 'Paris', '1981-10-23', '0128654872', 'Daseline'),
+(' ', 'Fribou', 'Charles', '19 avenue Fontaine', '75017', 'Paris', '1982-12-31', '0124169584', 'Wifangre'),
+(' ', 'Lark', 'Franck', '46 rue Iguon', '75009', 'Paris', '1980-04-24', '0164128598', 'GFIs Info'),
+(' ', 'Swen', 'Edouard', '83 boulevard Creju', '75004', 'Paris', '1968-05-29', '0146958421', 'Ounirt'),
+(' ', 'Gromart', 'Laurence', '195 rue de l\'union', '75002', 'Paris', '1967-08-13', '0142516438', 'Krimun'),
+(' ', 'Sharif', 'Mickael', '99 rue Rikoto', '75008', 'Paris', '1969-02-28', '0156942158', 'Bratisla'),
+(' ', 'Lagare', 'Renaud', '03 rue Omnio', '75015', 'Paris', '1985-01-14', '0125468591', 'Egura');
