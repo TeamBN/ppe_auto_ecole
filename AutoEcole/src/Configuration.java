dcde09cefa01efa709901bc571201d01501333ed
chocoow <darid.jeremy@gmail.com>
2014-05-22 16:44:36 +0200

    Creation de la classe Configuration qui va simplifier la configuration de connexion a la BDD

---
+++ AutoEcole/src/Configuration.java
@@ -0,0 +1,23 @@
+
+public class Configuration {
+
+		private static String serveur="localhost", bdd="autoecole", user="root", mdp="";
+		
+		public static String getServeur ()
+		{
+			return serveur;
+		}
+		
+		public static String getBdd ()
+		{
+			return bdd;
+		}
+		public static String getUser ()
+		{
+			return user;
+		}
+		public static String getMdp ()
+		{
+			return mdp;
+		}
+}
