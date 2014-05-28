4a0739f3af318811b46e4411e9f3543ec0352bac
chocoow <darid.jeremy@gmail.com>
2014-05-28 12:21:32 +0200

    Revert "Revert "Creation des méthodes Voitures""
    This reverts commit 6151000bab026c7860554aeaaf3d318bab5ce473.

--- AutoEcole/src/Lecon.java
+++ AutoEcole/src/Lecon.java
@@ -10,13 +10,15 @@ import javax.swing.event.ListSelectionListener;
 public class Lecon 
 {
 	private LinkedList<Moniteur> LesMoniteurs;
+	private LinkedList<Voiture> LesVoitures;
 	
 	public Lecon()
 	{
 		this.LesMoniteurs = new LinkedList<Moniteur>();
+		this.LesVoitures = new LinkedList<Voiture>();
 	}
 	
-	/* Definition des méthodes pour le moniteur */
+	/********* Definition des méthodes pour le moniteur ***********/
 	public void chargerMoniteurs()
 	{
 		String requete = "Select * from moniteur;";
@@ -76,23 +78,6 @@ public class Lecon
 			}
 			return null;
 		}
-	public static void supprimerMoniteur (int code)
-	{
-		String requete ="";
-			try
-			{
-				BDD uneBDD = new BDD ();
-				uneBDD.seConnecter();
-				Statement unStat = uneBDD.getMaConnexion().createStatement();
-				unStat.execute(requete);
-				unStat.close();
-				uneBDD.seConnecter();
-			}
-			catch(SQLException exp)
-			{
-				System.out.println("Erreur d'execution de la requete:" + requete);
-			}
-	}
 
 		public void insererMoniteur(Moniteur unMon)
 		{
@@ -133,6 +118,24 @@ public class Lecon
 				}
 		}
 		
+		public static void supprimerMoniteur (int code)
+		{
+			String requete ="";
+				try
+				{
+					BDD uneBDD = new BDD ();
+					uneBDD.seConnecter();
+					Statement unStat = uneBDD.getMaConnexion().createStatement();
+					unStat.execute(requete);
+					unStat.close();
+					uneBDD.seConnecter();
+				}
+				catch(SQLException exp)
+				{
+					System.out.println("Erreur d'execution de la requete:" + requete);
+				}
+		}
+		
 		
 		public void afficherLesMoniteurs ()
 		{
@@ -146,4 +149,100 @@ public class Lecon
 		public LinkedList<Moniteur> getLesMoniteurs(){
 	        return this.LesMoniteurs;
 		}
+		
+		/********* Definition des méthodes pour le moniteur ***********/
+		public void chargerVoitures()
+		{
+			String requete = "Select * from voiture;";
+			this.LesVoitures.clear();
+			
+			try
+			{
+				BDD uneBDD = new BDD();
+				uneBDD.seConnecter();
+				Statement unStat = uneBDD.getMaConnexion().createStatement();
+				ResultSet unRes = unStat.executeQuery(requete);
+				
+				while (unRes.next())
+				{
+					int idv = unRes.getInt("idv");
+					String immatriculation = unRes.getString("immatriculation");
+					String modele = unRes.getString("modele");
+					String dateachat = unRes.getString("date achat");
+					int nbrkm = unRes.getInt("nbrkm");
+					float conso = unRes.getFloat("conso");
+					
+					Voiture uneVoiture = new Voiture(idv, immatriculation, modele, dateachat, nbrkm, conso);
+					this.LesVoitures.add(uneVoiture);
+					
+				}
+				unRes.close();
+				unStat.close();
+				uneBDD.seDeconnecter();
+			}
+			catch (SQLException exp)
+			{
+				System.out.println("Erreur d'execution de la requête"+requete);
+			}
+		}
+		public void insererVoiture(Voiture uneVoit)
+		{
+			String requete ="insert into voiture (immatriculation, modele, dateachat, nbrkm, conso) values ('";
+			requete += uneVoit.getImmatriculation()+"', '"+uneVoit.getModele()+"', '"+uneVoit.getDateachat()+"', '"+uneVoit.getNbrkm()+"', '"+uneVoit.getConso()+"');";
+				try
+				{
+					BDD uneBDD = new BDD ();
+					uneBDD.seConnecter();
+					Statement unStat = uneBDD.getMaConnexion().createStatement();
+					unStat.execute(requete);
+					unStat.close();
+					uneBDD.seConnecter();
+				}
+				catch(SQLException exp)
+				{
+					System.out.println("Erreur d'execution de la requete:" + requete);
+				}
+		}
+		
+		public void modifierVoiture(Voiture uneVoit)
+		{
+			String requete ="Update voiture set immatriculation='"+uneVoit.getImmatriculation()+"', modele='"+uneVoit.getModele()+"', ";
+			requete += "dateachat='"+uneVoit.getDateachat()+"', nbrkm='"+uneVoit.getNbrkm()+"', conso='"+uneVoit.getConso()+"'WHERE idv="+uneVoit.getIdv()+";";
+				try
+				{
+					BDD uneBDD = new BDD ();
+					uneBDD.seConnecter();
+					Statement unStat = uneBDD.getMaConnexion().createStatement();
+					unStat.execute(requete);
+					unStat.close();
+					uneBDD.seConnecter();
+				}
+				catch(SQLException exp)
+				{
+					System.out.println("Erreur d'execution de la requete:" + requete);
+				}
+		}
+		
+		public static void supprimerVoiture (int code)
+		{
+			String requete ="";
+				try
+				{
+					BDD uneBDD = new BDD ();
+					uneBDD.seConnecter();
+					Statement unStat = uneBDD.getMaConnexion().createStatement();
+					unStat.execute(requete);
+					unStat.close();
+					uneBDD.seConnecter();
+				}
+				catch(SQLException exp)
+				{
+					System.out.println("Erreur d'execution de la requete:" + requete);
+				}
+		}
+			
+			public LinkedList<Voiture> getLesVoitures(){
+		        return this.LesVoitures;
+			}
+			
 }
