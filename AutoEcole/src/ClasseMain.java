ec9ec3927a856d8400e0d776cd983b345eb7e468
chocoow <darid.jeremy@gmail.com>
2014-06-01 00:33:02 +0200

    Simplification du code d'instanciation

--- AutoEcole/src/ClasseMain.java
+++ AutoEcole/src/ClasseMain.java
@@ -6,15 +6,17 @@ public class ClasseMain {
 	public static void main (String[] args)
 	{		   	   
 		
-		
 		try {
 			  UIManager.setLookAndFeel("com.sun.java.swing.plaf.windows.WindowsLookAndFeel");
+			  
 
-			} catch (Exception e) {
-			  }
+			} 
+		catch (Exception e) 
+			{
+			
+			}
 		
-		Fenetre fen = new Fenetre();
-				
+		new Fenetre();			
 			
 	}
 }
